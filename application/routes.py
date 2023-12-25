9e55b0753d40b614b8157c6a11e6f6276ef14585
timothyho12321 <timhotim2@gmail.com>
2023-12-25 13:39:19 +0800

    add category chart

--- application/routes.py
+++ application/routes.py
@@ -14,12 +14,31 @@ def dashboard():
     
     income_vs_expense = db.session.query(db.func.sum(IncomeExpense.amount), IncomeExpense.type).group_by(IncomeExpense.type).order_by(IncomeExpense.type).all()
 
+    dates = db.session.query(db.func.sum(IncomeExpense.amount),IncomeExpense.date).group_by(IncomeExpense.date).order_by(IncomeExpense.date).all()
+    
+    category_comparison = db.session.query(db.func.sum(IncomeExpense.amount),IncomeExpense.category).group_by(IncomeExpense.category).order_by(IncomeExpense.category).all()
+
+    income_category = []
+    for amounts, _ in category_comparison:
+        income_category.append(amounts)
+
     income_expense = []
     for total_amount, _ in income_vs_expense:
         income_expense.append(total_amount)
+
+    over_time_expenditure = []
+    dates_labels = []
+    for amount, date in dates:
+        over_time_expenditure.append(amount)
+        dates_labels.append(date.strftime("%m-%d-%Y"))
+
+
    
     return render_template('dashboard.html',
-                           income_vs_expenses=json.dumps(income_expense))
+                           income_vs_expenses=json.dumps(income_expense),
+                           over_time_expenditure=json.dumps(over_time_expenditure),
+                           dates_labels=json.dumps(dates_labels),
+                           category_spend = json.dumps(income_category))
 
 @app.route("/add", methods =["GET", "POST"])
 def add_expense():
